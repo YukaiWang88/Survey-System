0187d10790a1745431b1874046cf6dc9ed9292ca
HaleyKwok <kwokhinchi@gmail.com>
2025-04-21 19:32:49 +0800

    tried render for backend deployment

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,11 +1,20 @@
 import axios from 'axios';
 
 // Create a configured axios instance
+// const API = axios.create({
+//   baseURL: 'http://localhost:3000/api', //TODO process.env.base ? or just ./api
+//   headers: {
+//     'Content-Type': 'application/json'
+//   }
+// });
+
+
 const API = axios.create({
-  baseURL: 'http://localhost:3000/api', //TODO process.env.base ? or just ./api
+  baseURL: process.env.REACT_APP_API_URL || 'https://survey-system-api-l693.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
-  }
+  },
+  withCredentials: true
 });
 
 // Add auth token automatically to every request
